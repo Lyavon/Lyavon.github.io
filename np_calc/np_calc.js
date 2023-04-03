6a58d3c11447e070a205e13b7ce973dd867a54b1
Leonid Ragunovich <leonid.makintosh@yandex.by>
2023-04-04 00:23:41 +0300

    Initial draft of calc bootstrap and oxygenation_index.

---
+++ np_calc/np_calc.js
@@ -0,0 +1,17 @@
+const get = (id) => document.querySelector(id);
+
+let head = get('head');
+
+let polyfill = document.createElement('script');
+polyfill.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
+head.appendChild(polyfill);
+
+let mathjax = document.createElement('script');
+mathjax.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+head.appendChild(mathjax);
+
+let css = document.createElement('link');
+css.type = 'text/css';
+css.rel = 'stylesheet';
+css.href = 'https://lyavon.github.io/np_calc/np_calc.css';
+head.appendChild(css);
