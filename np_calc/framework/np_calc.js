f0b39916054c3aad246b67c01a670b06761d2a01
Leonid Ragunovich <leonid.makintosh@yandex.by>
2023-04-05 02:20:06 +0300

    Add bootstrap to np_calc. Use it in oxygenation_index.

--- np_calc/framework/np_calc.js
+++ np_calc/framework/np_calc.js
@@ -2,16 +2,17 @@ const get = (id) => document.querySelector(id);
 
 let head = get('head');
 
-let polyfill = document.createElement('script');
-polyfill.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
-head.appendChild(polyfill);
-
 let mathjax = document.createElement('script');
 mathjax.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
 head.appendChild(mathjax);
 
-let css = document.createElement('link');
-css.type = 'text/css';
-css.rel = 'stylesheet';
-css.href = 'https://lyavon.github.io/np_calc/framework/np_calc.css';
-head.appendChild(css);
+let bootstrap_meta = document.createElement('meta');
+bootstrap_meta.name = 'viewport';
+bootstrap_meta.content = 'width = device-width, initial-scale = 1';
+head.appendChild(bootstrap_meta);
+
+let extra_css = document.createElement('link');
+extra_css.type = 'text/css';
+extra_css.rel = 'stylesheet';
+extra_css.href = 'https://lyavon.github.io/np_calc/framework/np_calc.css';
+head.appendChild(extra_css);
