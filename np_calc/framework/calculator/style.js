faa0b4b77084a16c182e1bae19917aa865898778
Leonid Ragunovich <leonid.makintosh@yandex.by>
2023-04-10 15:23:16 +0300

    Implement calculator.

---
+++ np_calc/framework/calculator/style.js
@@ -0,0 +1,33 @@
+import Calc from '../primitives.js';
+import '../style.js';
+
+Calc.addCssClass(
+  'calculator_container',
+  '',
+  [
+    'container-fluid',
+    'border',
+    'rounded',
+    'border-secondary',
+    'p-0',
+    'bg-light',
+  ],
+);
+Calc.addCssClass(
+  'calculator_row',
+  '',
+  [
+    'row',
+    'p-0',
+    'm-0',
+  ],
+);
+Calc.addCssClass(
+  'calculator_col',
+  '',
+  [
+    'col-12',
+    'p-0',
+    'm-0',
+  ],
+);
