f9b487ec967bc6f89a3de00270950ebcafac98f2
Leonid Ragunovich <leonid.makintosh@yandex.by>
2023-04-10 15:20:30 +0300

    Implement equation.

---
+++ np_calc/framework/equation/style.js
@@ -0,0 +1,42 @@
+import Calc from '../primitives.js';
+import '../style.js';
+
+Calc.addCssClass(
+  'equation_container',
+  '',
+  [
+    'container-fluid',
+    'p-0',
+    'm-0',
+    'border',
+    'border-light',
+    'bg-white',
+    'rounded',
+  ],
+);
+Calc.addCssClass(
+  'equation_row',
+  '',
+  [
+    'row',
+    'm-0',
+  ],
+);
+Calc.addCssClass(
+  'equation_column',
+  '',
+  [
+    'col-12',
+    'p-0',
+  ],
+);
+Calc.addCssClass(
+  'equation_content',
+  '',
+  [
+    'fs-2',
+    'fw-bold',
+    'text-center',
+    'p-2',
+  ],
+);
