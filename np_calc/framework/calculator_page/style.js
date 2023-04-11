153ff2a12d8c0d7e09ab642e9b75bda28f73e954
Leonid Ragunovich <leonid.makintosh@yandex.by>
2023-04-12 01:32:22 +0300

    Implement calculator_page.

---
+++ np_calc/framework/calculator_page/style.js
@@ -0,0 +1,33 @@
+import Calc from "../primitives.js";
+import '../style.js';
+
+Calc.addCssClass(
+  'calculator_page_container',
+  '',
+  [
+    'border',
+    'border-primary',
+    'container-fluid',
+    'm-0',
+    'p-0',
+    'rounded',
+  ],
+);
+Calc.addCssClass(
+  'calculator_page_row',
+  '',
+  [
+    'row',
+    'm-1',
+    'p-0',
+  ],
+);
+Calc.addCssClass(
+  'calculator_page_column',
+  '',
+  [
+    'col-12',
+    'p-0',
+    'm-0',
+  ],
+);
