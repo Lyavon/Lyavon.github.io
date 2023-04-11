9e3acec0e8fbf211292f70b8334e27c52c2a1fff
Leonid Ragunovich <leonid.makintosh@yandex.by>
2023-04-12 00:27:47 +0300

    Remove logging. Use createIframe in the calculator template.

--- np_calc/framework/page.js
+++ np_calc/framework/page.js
@@ -4,13 +4,11 @@ import './style.js';
 let id = undefined;
 let parent = undefined;
 let container = Calc.get('#root');
-console.log(container);
 
 Calc.setMessageHandler((src, msg) => {
   if (!('event' in msg))
     return;
   id = msg.id;
-    console.log('setup', id);
   parent = src;
   Calc.sendMessage(
     parent,
@@ -26,7 +24,6 @@ window.addEventListener(
   (e) => {
     if (!parent || !id)
       return;
-    console.log('resize', id);
     Calc.sendMessage(
       parent,
       {
